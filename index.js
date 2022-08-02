a22e90676179c494d3e02429e6d65efbb5dc5f9e
Bilal.Khalid <bilal.khalid@10pearls.com>
2022-08-02 12:06:13 +0500

    added cors config

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://desolate-mountain-37815.herokuapp.com');
+  res.setHeader('Access-Control-Allow-Origin', 'https://bilalkhalid-10p.github.io');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
