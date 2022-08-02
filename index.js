8965f713d570301d15e41cea8c10bd297d74d4ae
Bilal.Khalid <bilal.khalid@10pearls.com>
2022-08-02 14:29:53 +0500

    connected db and query table data

--- index.js
+++ index.js
@@ -47,3 +47,37 @@ app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
 });
+
+const { Client } = require('pg');
+
+var connectionString = "postgres://ftopzbzlusikbd:7f967c819a8cf5bec97b50d5d3b489ab78441780f1e38f87ccd775f33d31bc5c@ec2-34-227-135-211.compute-1.amazonaws.com:5432/daibac7pl3rgn7"
+
+const client = new Client({
+  connectionString: connectionString,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
+client.query(`SELECT * FROM Users;`, (err, res) => {
+  if (err) {
+    console.log("Error - Failed to select all from Users");
+    console.log(err);
+  }
+  else {
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
+  }
+});
