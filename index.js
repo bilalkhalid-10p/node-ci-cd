8a92a57eadf4ed534c7e94aec7569c2e2a2a616b
Bilal.Khalid <bilal.khalid@10pearls.com>
2022-08-02 11:37:32 +0500

    added new end point

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ app.get('/', (req, res) => {
     .send('Hello Bilal server is running on 8080')
     .end();
 });
+
+app.get('/data', function(req, res) {
+  res.json([{
+    number: 1,
+    name: 'John',
+    gender: 'male'
+  },
+    {
+      number: 2,
+      name: 'Ashley',
+      gender: 'female'
+    }
+  ]);
+});
  
 // Start the server
 const PORT = process.env.PORT || 8080;
