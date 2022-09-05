64e3583c120aedcf79199f1a7375a26e83e4e010
Bilal.Khalid <bilal.khalid@10pearls.com>
2022-09-05 16:48:04 +0500

    updated create user

--- index.js
+++ index.js
@@ -93,19 +93,19 @@ app.get('/managers', function(req, res) {
 });
 
 app.post('/create', function(req, res) {
-    console.log(req.body);
-    res.send('BODY ==> ' + JSON.stringify(req.body));
+    // console.log(req.body);
+    // res.send('BODY ==> ' + JSON.stringify(req.body));
 
-    // client.query((`INSERT INTO "users" ("first_name", "last_name", "manager_id")
-    //                   VALUES ($1, $2)`, [req.body.first_name, req.body.last_name, req.body.manager_id]), (err, result) => {
-    //     if (err) {
-    //         console.log("Error - Failed to insert data into Users");
-    //         console.log(err);
-    //     }
-    //     else {
-    //         res.status(200).json(result.rows)
-    //     }
-    // });
+    client.query((`INSERT INTO "users" ("first_name", "last_name", "manager_id")
+                      VALUES ($1, $2)`, [req.body.data.first_name, req.body.data.last_name, req.body.data.manager_id]), (err, result) => {
+        if (err) {
+            console.log("Error - Failed to insert data into Users");
+            console.log(err);
+        }
+        else {
+            res.status(200).json(result.rows)
+        }
+    });
 });
 
 
