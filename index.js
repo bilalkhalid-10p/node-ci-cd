de6606bcada7c66dd641d8f7cfcef0a46dbd8923
Bilal.Khalid <bilal.khalid@10pearls.com>
2022-09-06 12:13:18 +0500

    updated create user

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect;
-  res.setHeader('Access-Control-Allow-Origin', 'https://vue-ci-cd.herokuapp.com, http://localhost:8080/');
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
